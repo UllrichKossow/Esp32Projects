7c26bf01eeaa8c871228a1dfe344f6ca76bbab4b
Ullrich Kossow <ullrich.kossow@rohde-schwarz.com>
2020-06-07 21:31:32 +0200

    Some BME280 work.

--- i2c_tools/main/measure.cc
+++ i2c_tools/main/measure.cc
@@ -1,7 +1,7 @@
 
-
 #include <cstdint>
 
+#include "driver/i2c.h"
 #include "bme280.h"
 
 int8_t user_i2c_read(uint8_t id, uint8_t reg_addr, uint8_t *data, uint16_t len)
@@ -18,8 +18,46 @@ void user_delay_ms(uint32_t period)
 {
 	
 }
+//-----------------------------------------------------------------------------------------------------
+#define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
+#define WRITE_BIT I2C_MASTER_WRITE  /*!< I2C master write */
+#define READ_BIT I2C_MASTER_READ    /*!< I2C master read */
+#define ACK_CHECK_EN 0x1            /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS 0x0           /*!< I2C master will not check ack from slave */
+#define ACK_VAL 0x0                 /*!< I2C ack value */
+#define NACK_VAL 0x1                /*!< I2C nack value */
 
+uint32_t i2c_frequency = 100000;
+static i2c_port_t i2c_port = I2C_NUM_0;
 
+static esp_err_t i2c_master_driver_initialize(void)
+{
+    i2c_config_t conf;
+	conf.mode = I2C_MODE_MASTER;
+	conf.sda_io_num = 21;
+	conf.scl_io_num = 22;
+	conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+	conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+	conf.master.clk_speed = i2c_frequency;
+    return i2c_param_config(0, &conf);
+}
+
+void x()
+{
+    i2c_driver_install(i2c_port, I2C_MODE_MASTER, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+    i2c_master_driver_initialize();
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, 0x78 << 1 | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_port, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+i2c_driver_delete(i2c_port);
+    
+}
+
+//-----------------------------------------------------------------------------------------------------
 void measure(void)
 {
     struct bme280_dev dev;
