6afb91751fc13637f0a4f8c8503e55b90c0d3d97
Ullrich Kossow <ullrich.kossow@web.de>
2020-08-03 11:14:16 +0200

    Begin DataCollector

--- room_meter/main/bme280_access.cc
+++ room_meter/main/bme280_access.cc
@@ -25,7 +25,7 @@ using namespace std;
 static const char *TAG = "measure";
 
 
-int8_t user_i2c_read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t len)
+static int8_t user_i2c_read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t len)
 {
     esp_err_t espRc;
 
@@ -39,19 +39,19 @@ int8_t user_i2c_read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t
     i2c_master_write_byte(cmd, (dev_addr << 1) | I2C_MASTER_READ, true);
 
     if (len > 1) 
-	{
+    {
         i2c_master_read(cmd, data, len-1, I2C_MASTER_ACK);
     }
     i2c_master_read_byte(cmd, data+len-1, I2C_MASTER_NACK);
     i2c_master_stop(cmd);
-
+    
     espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
     i2c_manager::instance()->ReleaseCmdHandle(cmd);
 
     return (espRc == ESP_OK) ? 0 : -1;
 }
 
-int8_t user_i2c_write(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t len)
+static int8_t user_i2c_write(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t len)
 {
     esp_err_t espRc;
     i2c_cmd_handle_t cmd = i2c_manager::instance()->GetCmdHandle();
@@ -70,7 +70,7 @@ int8_t user_i2c_write(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_
     return (espRc == ESP_OK) ? 0 : -1;
 }
 
-void user_delay_ms(uint32_t msek)
+static void user_delay_ms(uint32_t msek)
 {
     vTaskDelay(msek/portTICK_PERIOD_MS);
 }
