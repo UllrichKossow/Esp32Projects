adc35375b6817604f783e248a92867b0127b4466
Ullrich Kossow <ullrich.kossow@web.de>
2020-12-29 18:23:47 +0100

    Cleanup

--- Play/main/app_main.cc
+++ Play/main/app_main.cc
@@ -12,6 +12,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include <esp_system.h>
+#include <esp_timer.h>
 #include <driver/gpio.h>
 #include <esp_log.h>
 
@@ -20,8 +21,8 @@
 
 #define TAG "app"
 
-const gpio_num_t LED_CLK = GPIO_NUM_5;
-const gpio_num_t LED_DTA = GPIO_NUM_4;
+const gpio_num_t LED_CLK = GPIO_NUM_4;
+const gpio_num_t LED_DTA = GPIO_NUM_5;
 
 void lcd_tm1637_task(void * arg)
 {
@@ -33,15 +34,16 @@ void lcd_tm1637_task(void * arg)
 	tm1637_set_brightness(lcd, 1);	
 	while (true)
 	{
-		// Get current system time
-		time_t now = 0;
+		time_t now;
 		struct tm timeinfo;
 		time(&now);
 		localtime_r(&now, &timeinfo);
 		int time_number = 100 * timeinfo.tm_hour + timeinfo.tm_min;
 
 		tm1637_set_number_lead_dot(lcd, time_number, true, timeinfo.tm_sec % 2 ? 0xFF : 0x00);
-		vTaskDelay(100 / portTICK_RATE_MS);
+		int64_t t = esp_timer_get_time();
+		//ESP_LOGI(TAG, "t=%lli", t);
+		vTaskDelay(1000 / portTICK_RATE_MS);
 	}
 }
 
