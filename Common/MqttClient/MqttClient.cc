9748db35723a16b090bf9b9f49fd1d3dcdc09b29
Ullrich Kossow <ullrich.kossow@web.de>
2021-10-13 20:01:24 +0200

    Use also domain name for mqtt (bpi.fritz.box).

--- Common/MqttClient/MqttClient.cc
+++ Common/MqttClient/MqttClient.cc
@@ -36,7 +36,7 @@ void MqttClient::init()
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
         esp_mqtt_client_config_t mqtt_cfg = {
-                .uri = "mqtt://bpi",
+                .uri = "mqtt://bpi.fritz.box",
         };
 #pragma GCC diagnostic pop
 
