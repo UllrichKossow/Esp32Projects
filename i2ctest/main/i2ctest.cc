19f499c622c5cc9136a5d134a6b918e8193fc75a
Ullrich Kossow <ullrich.kossow@web.de>
2025-03-12 22:21:09 +0100

    WIP

--- i2ctest/main/i2ctest.cc
+++ i2ctest/main/i2ctest.cc
@@ -1,13 +1,20 @@
-#include <stdio.h>
+#include <cstdio>
 
 const char *TAG="i2ctest";
 
 #include "esp_log.h"
+#include "driver/i2c_master.h"
+
+
+void i2ctest() {
+
+}
 
 extern "C" void app_main(void);
 void app_main(void)
 {
     ESP_LOGI(TAG, "Start...");
+    i2ctest();
     while (1)
     {
         
