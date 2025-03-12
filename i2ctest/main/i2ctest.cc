4e3e5fa64da6365b1b6d10660d9ffd535701edc9
user <user@debdev>
2025-03-12 21:35:53 +0100

    i2ctest

---
+++ i2ctest/main/i2ctest.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+const char *TAG="i2ctest";
+
+#include "esp_log.h"
+
+extern "C" void app_main(void);
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Start...");
+    while (1)
+    {
+        
+    }
+}
